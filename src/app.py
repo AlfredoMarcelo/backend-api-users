2b036614cb6ee82d2dfc2729e86090d80f454c12
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-08-10 22:00:52 -0400

    Se crea la base de datos, metodos funcionando get, post, put, delete

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify,request
 from flask_migrate import Migrate
-from models import db, User
+from models import db, User, Profile
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -21,27 +21,31 @@ def main():
 @app.route('/api/users', methods=['GET'])
 def get_users():
     users = User.query.all()
-    users = list(map(lambda user: user.serialize(), users))
+    users = list(map(lambda user: user.serialize_with_profile(), users))
     return jsonify(users), 200
 
-@app.route('/api/create', methods=['POST'])
+@app.route('/api/user/create', methods=['POST'])
 def create_user():
     
     name = request.json.get("name", None)
-    if name == None or name == "":
-        return jsonify({"msg":"debe ingresar un nombre"}), 400
+    if name == None or name == "": return jsonify({"msg":"debe ingresar un nombre"}), 400
     
     lastname = request.json.get("lastname", None)
-    if lastname == None or lastname == "":
-        return jsonify({"msg":"debe ingresar un apellido"}),400
+    if lastname == None or lastname == "": return jsonify({"msg":"debe ingresar un apellido"}),400
     
     email = request.json.get("email", None)
-    if email == None or email == "":
-        return jsonify({"msg":"debe ingresar un email"}),400
+    user_db = User.query.filter_by(email=email).first()
+    if user_db: return jsonify({'msg': 'el email esta registrado'}), 400
+    if email == None or email == "": return jsonify({"msg":"debe ingresar un email"}),400
     
     password = request.json.get("password", None)
-    if password == None or password == "":
-        return jsonify({"msg":"debe ingresar un password"}), 400
+    if password == None or password == "": return jsonify({"msg":"debe ingresar un password"}), 400
+    
+    bio = request.json.get("bio", "")
+    twitter = request.json.get("twitter", "")
+    facebook = request.json.get("facebook", "")
+    instagram = request.json.get("instagram", "")
+    linkedin = request.json.get("linkedin", "")
     
     # Se instancia una nueva clase User
     user = User()
@@ -51,9 +55,44 @@ def create_user():
     user.email = email
     user.password = password
     # se importa la funcion de la clase User, que agrega y guarda lo seteado
+    
+    profile = Profile()
+    profile.bio = bio
+    profile.twitter = twitter
+    profile.facebook = facebook
+    profile.instagram = instagram
+    profile.linkedin = linkedin
+    
+    user.profile = profile  #se usa el relationship, este asigna en el model la id del usuario
     user.save()
     
     return jsonify({"msg":"user creado"}), 201
+
+@app.route('/api/user/edit/<int:id>', methods=['PUT'])
+def actualizar(id):
+    datos = request.get_json()
+    
+    name = datos['name']
+    lastname = datos['lastname']
+    email = datos['email']
+    password = datos['password']
+    
+    user = User.query.get(id)
+    print (user.name)
+    user.name = name
+    user.lastname = lastname
+    user.email = email
+    user.password = password
+    user.update()
+    return jsonify({'msg': 'usuario actualizado'}), 200
+
+@app.route("/api/user/delete/<int:id>", methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if not user:
+        return jsonify({'msg':'el usuario no existe'}), 404
+    user.delete()
+    return jsonify({'msg': 'El usuario fue eliminado'}), 200
 
 if __name__ == '__main__':
     app.run()
--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,7 +10,8 @@ class User(db.Model):
     lastname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100),nullable=False)
-    
+    profile = db.relationship('Profile', backref='user', uselist=False)
+
     def serialize(self):
         return {
             "id": self.id,
@@ -18,7 +20,15 @@ class User(db.Model):
             "email": self.email,
             "password": self.password
         }
-    
+    def serialize_with_profile(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "password": self.password,
+            "profile": self.profile.serialize()
+        }
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -26,6 +36,38 @@ class User(db.Model):
     def update(self):
         db.session.commit()
         
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    bio = db.Column(db.Text, default="")    #default para dar un valor por defecto al campo
+    twitter = db.Column(db.String(100), default="")
+    facebook = db.Column(db.String(100), default="")
+    instagram = db.Column(db.String(100), default="")
+    linkedin = db.Column(db.String(100), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "bio": self.bio,
+            "twitter": self.twitter,
+            "facebook": self.facebook,
+            "instagram": self.instagram,
+            "linkedin": self.linkedin
+        }
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+    
     def delete(self):
         db.session.delete(self)
         db.session.commit()
